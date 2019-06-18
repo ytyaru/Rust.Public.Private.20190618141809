42e05144962ec350f1eb36d732972a561d28c4d5
ytyaru <ytyaru@outlook.jp>
2019-06-18 14:37:09 +0900

    privateは同モジュール内でなら参照可

---
+++ src/1/main.rs
@@ -0,0 +1,12 @@
+/*
+ * RustのPublic,Private。
+ * CreatedAt: 2019-06-18
+ */
+fn main() {
+    println!("{}", my_mod::public());
+//    println!("{}", my_mod::private()); // error[E0603]: function `private` is private
+}
+mod my_mod {
+    pub fn public() -> String { private(); String::from("my_mod::public()") }
+    fn private() -> String { String::from("my_mod::private()") }
+}
